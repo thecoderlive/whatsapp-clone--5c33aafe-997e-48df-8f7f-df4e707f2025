4e07fa02aaece097b8668fb6421751d578c9e0f8
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-04-14 15:56:34 +0530

    Initial commit for file: App.js

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Button, Text, View, StyleSheet } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+
+import Chatsscreen from './Chatsscreen'
+
+const Root = createStackNavigator()
+
+export default function App() {
+return (
+<NavigationContainer>
+<Root.Navigator
+    initialRouteName="Chatsscreen"
+    screenOptions={{
+        headerMode:'screen',
+        headerStyle: { backgroundColor: 'whitesmoke', height: 40 },
+        title: null,
+        cardOverlayEnabled:true,
+        cartStyle:{flex:1},
+        animationEnabled:true,
+        gestureEnabled:true
+    }}
+    >
+<Root.Screen name="Chatsscreen" component={Chatsscreen} />
+</Root.Navigator>
+<Text style={{height: 1}}>.</Text>
+</NavigationContainer>
+)}
+
+const styles = StyleSheet.create({
+  screen: {
+    marginTop: 40,
+    alignItems: 'center',
+  },
+})
